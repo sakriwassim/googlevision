3186b01b63d925772e8dbba9e261c94d025b3a60
sakriwassim <sakriwassim@gmail.com>
2024-06-08 01:39:25 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/googlevision/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.googlevision
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/HomeScreen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'TextScanner.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>TextScanner()));
+          },
+          child: const Text('Open Camera to scan'),
+        ),
+      ),
+    );
+  }
+}
